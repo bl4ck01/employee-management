60970e7bc712403a84f3a66050006a5551606916
navinspm <navinspm@gmail.com>
2014-09-24 16:46:11 +0530

    added dependent destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,4 +36,5 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+   has_many :employees, dependent: :destroy
 end
